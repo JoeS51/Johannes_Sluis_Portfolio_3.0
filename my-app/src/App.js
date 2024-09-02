3447efb1755122eab06a02eb9c9adb03b052e92e
Johannes Sluis <joesluis51@gmail.com>
2024-09-03 07:25:39 +0900

    first draft

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import { useTrail, a } from '@react-spring/web'
 
-function App() {
+import styles from './styles.module.css'
+
+import Trail from './Components/Trail'
+import { Parallax, ParallaxLayer } from '@react-spring/parallax'
+import ResponsiveAppBar from './Components/Navbar'
+
+export default function App() {
+  const [open, set] = useState(true)
+  const alignCenter = { display: 'flex', alignItems: 'center' }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ResponsiveAppBar></ResponsiveAppBar>
+      <Parallax pages={3}>
+        <ParallaxLayer offset={0} speed={0} style={{ ...alignCenter, justifyContent: 'center' }}>
+        <div className={styles.container}>
+          <Trail open={open}>
+            <span>Johannes</span>
+            <span>Sluis</span>
+            <span>Portfolio</span>
+          </Trail>
+        </div>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={1} speed={0} style={{ ...alignCenter, justifyContent: 'flex-end', backgroundColor: '#ebebeb'}}>
+          <div className={`${styles.card} ${styles.parallax} ${styles.purple}`}>
+            <p>I'm not</p>
+          </div>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={2} speed={0} style={{ ...alignCenter, justifyContent: 'flex-end', backgroundColor: '#ccd5c4' }}>
+          <div className={`${styles.card} ${styles.parallax} ${styles.blue}`}>
+            <p>Neither am I</p>
+          </div>
+        </ParallaxLayer>
+      </Parallax>
     </div>
-  );
+  )
 }
 
-export default App;
