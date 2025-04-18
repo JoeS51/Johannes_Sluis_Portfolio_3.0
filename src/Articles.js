13570147359cfb6cb2a66a3df1666223fba83bc2
Johannes Sluis <joesluis51@gmail.com>
2025-04-18 12:37:39 -0700

    trying to render md

--- src/Articles.js
+++ src/Articles.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ReactMarkdown from 'react-markdown';
+import md from './microsoft.md'
+
+const markdown = `
+# Hello 👋
+
+This is a **Markdown** _example_.
+- It supports lists
+- \`inline code\`
+`;
 
 export default function Articles() {
+    const [markdown, setMarkdown] = useState('test');
+
+    useEffect(() => {
+        fetch(md)
+            .then((res) => {
+                console.log(res);
+                return res.text();
+            })
+            .then((text) => setMarkdown(text));
+    }, []);
+
     return (
         <div>
             <h1>Articles</h1>
+            <ReactMarkdown>
+                {markdown}
+            </ReactMarkdown>
         </div>
     );
 }
