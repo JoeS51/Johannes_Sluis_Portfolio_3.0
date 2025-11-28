d4112fe2e7bbcc381a42721f535a7a5ebebd7db8
Joe Sluis <joesluis51@gmail.com>
2025-11-27 16:50:02 -0800

    creating react from scratch article

--- src/data/posts.js
+++ src/data/posts.js
@@ -4,11 +4,22 @@ const posts = [
     frontmatter: {
       title: 'Hello World',
       description: 'Kicking off the blog with thoughts on learning in public and what to expect.',
-      date: '2024-04-21',
+      date: '2025-10-21',
       tags: ['Personal', 'Meta'],
       cover: null
     },
     content: '',
+  },
+  {
+    slug: 'react-from-scratch',
+    frontmatter: {
+      title: 'Building React from Scratch',
+      description: 'I built a lightweight version of react.js from scratch for fun.',
+      date: '2025-11-01',
+      tags: ['Personal', 'Meta'],
+      cover: null
+    },
+    content: '[learnings here and link]',
   }
 ];
 
