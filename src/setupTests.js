ad3a5e13f91a5ab5ecb8fe6740ac143d30f30f7c
Johannes Sluis <joesluis51@gmail.com>
2026-03-03 03:02:55 -0800

    fix: migrate from CRA to Vite and resolve dependency vulnerabilities

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,26 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+class MockIntersectionObserver {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+
+global.IntersectionObserver = MockIntersectionObserver;
+window.IntersectionObserver = MockIntersectionObserver;
+
+HTMLCanvasElement.prototype.getContext = () => ({
+  canvas: document.createElement('canvas'),
+  beginPath: () => {},
+  lineWidth: 0,
+  strokeStyle: '',
+  lineTo: () => {},
+  stroke: () => {},
+  closePath: () => {},
+  fillStyle: '',
+  globalAlpha: 1,
+  fillRect: () => {},
+  filter: ''
+});
---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react({ include: /\.[jt]sx?$/ })],
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.js$/,
+    exclude: []
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx'
+      }
+    }
+  },
+  test: {
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.js',
+    globals: true
+  }
+});
