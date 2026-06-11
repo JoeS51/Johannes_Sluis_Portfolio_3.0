8ed1d2ef6f7f83c05d82bdc503960a4faf3b228b
Joe Sluis <joesluis51@gmail.com>
2026-06-10 20:07:55 -0700

    start next blog post on mvcc in postgres

---
+++ src/content/blog/hello-world.md
@@ -0,0 +1,16 @@
+---
+title: Hello World
+description: Kicking off the blog with thoughts on learning in public and what to expect.
+date: 2025-10-21
+tags:
+  - Personal
+  - Meta
+cover: null
+---
+
+## Motivation
+
+There are a multiple reasons why I'm starting this blog, but my number one reason is to document my learning and hopefully provide value to others. Also maybe make it on HackerNews one day.
+
+## What to expect
+
--- src/data/posts/index.js
+++ src/data/posts/index.js
@@ -1,8 +1,9 @@
 import buildingReactFromScratch from './building-react-from-scratch';
+import howMvccWorksInPostgresql from './how-mvcc-works-in-postgresql';
 // import rustAzureServiceBusEmailWorker from './rust-azure-service-bus-email-worker';
 import sshPortfolioGuide from './ssh-portfolio-guide';
 import vimTestBuffer from './vim-test-buffer';
 
-const posts = [vimTestBuffer, buildingReactFromScratch, sshPortfolioGuide];
+const posts = [vimTestBuffer, buildingReactFromScratch, sshPortfolioGuide, howMvccWorksInPostgresql];
 
 export default posts;
