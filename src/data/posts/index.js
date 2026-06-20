c21e198625b529e6ecab9ae0332886c772ac44c1
Johannes Sluis <joesluis51@gmail.com>
2026-06-19 19:36:13 -0700

    remove unused files

--- src/data/posts/index.js
+++ src/data/posts/index.js
@@ -1,5 +1,4 @@
 import buildingReactFromScratch from './building-react-from-scratch';
-// import howMvccWorksInPostgresql from './how-mvcc-works-in-postgresql';
 // import rustAzureServiceBusEmailWorker from './rust-azure-service-bus-email-worker';
 import mvccInPostgres from './mvcc-in-postgres';
 import sshPortfolioGuide from './ssh-portfolio-guide';
