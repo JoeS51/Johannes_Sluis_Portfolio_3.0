4a33407deaa98a522a3f397defd9e3e57106b72d
Joe <joesluis51@gmail.com>
2026-08-23 19:59:00 -0700

    intro done

--- src/data/posts/what-is-aws-aurora.js
+++ src/data/posts/what-is-aws-aurora.js
@@ -8,14 +8,27 @@ const post = {
     cover: null
   },
   content: `
-You might already know this, but AWS has a lot of database products(DynamoDB, RDS, ElastiCache, DocumentDB, etc) and among all of them, the most popular one is most likely Amazon Aurora. It becomes confusing because Aurora isn't just a single database offering, there are four different Aurora products, and each one is quite different. I read the Aurora DSQL and Aurora Limitless research papers, so I have a better understanding of the differences (and so you don't have to). I'll be focusing more on the differences in their architecture and design rather than on how application developers use them.
 
-To start off, there are four offerings under the Amazon Aurora umbrella:
+### Intro
+
+Among all the hyperscalers, AWS has the most database products. There's DynamoDB, RDS, ElastiCache, DocumentDB, etc and the list goes on. Among them, there's a popular one named Amazon Aurora. The confusing thing is that there are multiple database offerings that have the name Aurora in it. 
+
+In fact, you can argue there are four (or three if you count Aurora MySQL/PostgreSQL as the same) offerings under the Amazon Aurora umbrella:
 
 - [Aurora MySQL](https://docs.aws.amazon.com/AmazonRDS/latest/AuroraUserGuide/Aurora.AuroraMySQL.html)
 - [Aurora PostgreSQL](https://docs.aws.amazon.com/AmazonRDS/latest/AuroraUserGuide/Aurora.AuroraPostgreSQL.html)
 - [Aurora PostgreSQL Limitless Database](https://docs.aws.amazon.com/AmazonRDS/latest/AuroraUserGuide/limitless.html)
 - [Aurora DSQL](https://docs.aws.amazon.com/aurora-dsql/latest/userguide/what-is-aurora-dsql.html)
+
+Why did they name them all Aurora? Why did they create DSQL when they already had limitless? Do they just love Postgres? I read the Aurora DSQL and Aurora Limitless research papers to hopefully answer these questions. Also, I'll be focusing more on the differences in their architecture and design rather than on how application developers use them. I am [databasemaxxing](https://pthorpe92.dev/databasemaxxing/) after all.
+
+Note: I don't work for AWS and am not affiliated with them at all
+
+### Aurora MySQL/PostgreSQL
+This is the classic Aurora offering and the first in the Aurora series, GA'ing in 2015. If you're familiar with AlloyDB, HorizonDB, or Neon, then you can think of it as something similar to those systems. The basic idea is that your Postgres/MySQL database doesn't live on a single machine. Aurora disaggregates compute and storage (basically Query Processor and Storage engine are separated). There is a single writer compute instance and you can add multiple read replicas, which are all backed by the same distributed storage layer.
+
+
+
 `
 };
 
