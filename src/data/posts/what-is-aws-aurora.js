804600918a84cc82aa140659a82215cd4ef0345a
Joe <joesluis51@gmail.com>
2026-08-28 15:13:49 -0700

    small correction

--- src/data/posts/what-is-aws-aurora.js
+++ src/data/posts/what-is-aws-aurora.js
@@ -28,7 +28,9 @@ This is the classic Aurora offering and the first in the Aurora series. If you'r
 
 [[AURORA_ARCHITECTURE_ANIMATION]]
 
-This animation shows the flow for both read and write paths. Writes pass through a dedicated writer instance and reads can pass through any of the read instances (these can be scaled up/out). The writer receives SQL queries from clients and generates redo/WAL records for the changes. Once the transaction commits, Aurora sends these redo/WAL records to its shared distributed storage layer (not S3) and waits for a quorum (4/6 storage copies ack the write). The storage layer of this system is replicated across three different AZs with six copies of the data across the AZs.
+This animation shows the flow for both read and write paths. Writes pass through a dedicated writer instance and reads can pass through any of the read instances (these can be scaled up/out). 
+
+The writer sends redo/WAL records to the distributed storage layer as changes occur. When the transaction commits, Aurora waits for the required storage quorum (4/6 storage copies ack the write) before acknowledging the commit. The storage layer of this system is replicated across three different AZs with six copies of the data across the AZs, so it is fairly fault-tolerant.
 
 The single writer is the biggest limitation of classic Aurora. The single writer bottlenecks the write throughput of this system. You have the option to vertically scale your writer, but at some point, the vertical scaling won't be able to handle all your writes. At which point, you want to horizontally scale your writer. That leads us to Aurora Limitless.
 
@@ -43,7 +45,7 @@ The last component is a **control plane** that manages the life cycle of the sha
 
 In this animation, the client issues a read query to router 3. Router 3 then uses its hash function on the dog_ids + routing metadata to understand which shard each of them belongs to. The router then sends the reads to those shards, combines their results and returns the result to the client.
 
-You might be asking, "but what if one shard gets a majority of the requests?" That's where *shard splitting* comes in. Either the customer or the control plane notices that one of the shards needs more capacity and initiates a shard split. If a shard is already overloaded, the last thing that you want to do is make it read and rewrite all of its data into a new shard. Aurora Limitless avoids that initial overhead of copying data to a new shard through Aurora's storage level CoW mechansm.
+You might be asking, "but what if one shard gets a majority of the requests?" That's where *shard splitting* comes in. Either the customer or the control plane notices that one of the shards needs more capacity and initiates a shard split. If a shard is already overloaded, the last thing that you want to do is make it read and rewrite all of its data into a new shard. Aurora Limitless avoids that initial overhead of copying data to a new shard through Aurora's storage level CoW mechanism.
 
 There are other issues with sharding your data such as expensive joins. Limitless addresses some of these issues through **collocation**. With collocation, you can place related tables on the same shard to make JOINs less expensive. Additionally, Limitless supports three different table types: sharded, reference, and standard tables so tables can be distributed across the shards in different ways.
 
